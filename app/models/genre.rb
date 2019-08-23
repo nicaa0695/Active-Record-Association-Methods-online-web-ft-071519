2dde579f08df0418c287ac52fc3a0f14cc3f28c5
Veronica Lopez <nicaa0695@gmail.com>
2019-08-23 17:13:03 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -11,6 +11,7 @@ class Genre < ActiveRecord::Base
   end
 
   def all_artist_names
-    # return an array of strings containing every musician's name
+    self.artists.collect do |t|
+      t.name 
   end
 end
